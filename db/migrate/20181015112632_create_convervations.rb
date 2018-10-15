1b200f5bb61ffac6ce6a67d5ca28e5085b9163c6
yachnytskyi <yachnytskyi1992@gmail.com>
2018-10-15 14:50:49 +0300

    Added migrations conversations and messages, and create model conversation

---
+++ db/migrate/20181015112632_create_convervations.rb
@@ -0,0 +1,10 @@
+class CreateConvervations < ActiveRecord::Migration[5.2]
+  def change
+    create_table :convervations do |t|
+      t.integer :sender_id
+      t.integer :recipient_id
+
+      t.timestamps
+    end
+  end
+end
